13ae19897cf65cf204fd0e57aae90d1393459ac2
Bijendra Kotdwar <bijendra.kotdwar@gmail.com>
2026-04-12 16:13:03 +0530

    feat: Sentry error monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://df3ae5b25095791acb47086c5048b0c6@o4511206606503936.ingest.us.sentry.io/4511206626557952",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
